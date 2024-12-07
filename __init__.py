f341da1c5578ee18574ff34ce6eafa2244c5339a
ShaunSwain12 <125476143+ShaunSwain12@users.noreply.github.com>
2024-12-07 15:41:28 +0000

    Add files

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from colours import *
+from plot_settings import *
---
+++ colours.py
@@ -0,0 +1,10 @@
+# Hex colours
+
+walesgreen = "#50B04E"
+walesred = "#B92831"
+royalsocietyred = "#C22E49"
+uobred = "#BE2A2B"
+uobblue = "#2F67DB"
+uobgold = "#FBE780"
+swanseablue = "#689ED3"
+swanseabrown = "#AD6A4C"
